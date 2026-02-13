e38d90a4cee85c583194683bdc24676562028f19
AbhilashG12 <abhilashggg15@gmail.com>
2026-02-13 20:51:48 +0530

    initial

--- Components/Calculator.jsx
+++ Components/Calculator.jsx
@@ -1,3 +1,43 @@
+// import { useState } from "react"
+// import "./Calculator.css"
+
+// const Calculator = () => {
+//     const [val , setVal] = useState("");
+//     const calculated=()=>{
+//         setVal(eval(val).toString());
+//     }
+//     const clearAll=()=>{
+//         setVal("")
+//     }
+
+//   return (
+//     <div>
+//         <h3>{val}</h3>
+//       <div className="calc">
+//         <button className="item" onClick={() => setVal(val + "1")}>1</button>
+//         <button className="item" onClick={()=>setVal(val+"2")}>2</button>
+//         <button className="item" onClick={()=>setVal(val+"3")}>3</button>
+//         <button className="item" onClick={()=>setVal(val+"4")}>4</button>
+//         <button className="item" onClick={()=>setVal(val+"5")}>5</button>
+//         <button className="item" onClick={()=>setVal(val+"6")}>6</button>
+//         <button className="item" onClick={()=>setVal(val+"7")}>7</button>
+//         <button className="item" onClick={()=>setVal(val+"8")}>8</button>
+//         <button className="item" onClick={()=>setVal(val+"9")}>9</button>
+//         <button className="item" onClick={()=>setVal(val+"0")}>0</button>
+//         <button className="item" onClick={()=>setVal(val+"+")}>+</button>
+//         <button className="item" onClick={()=>setVal(val+"-")}>-</button>
+//         <button className="item" onClick={()=>setVal(val+"*")}>*</button>
+//         <button className="item" onClick={()=>setVal(val+"/")}>/</button>
+//         <button className="item" onClick={()=>calculated()}>=</button>
+//         <button className="item" onClick={()=>clearAll()}>C</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Calculator
+
+
 import { useState } from "react"
 import "./Calculator.css"
 
--- Components/Counter.jsx
+++ Components/Counter.jsx
@@ -1,3 +1,32 @@
+// import { useState } from "react"
+// import "./Counter.css"
+// const Counter = () => {
+//     const [count, setCount] = useState(0);
+//     const inc=()=>{
+//         setCount(count+1);
+//     }
+//     const dec=()=>{
+//         setCount(count-1);
+//     }
+//     const reset=()=>{
+//         setCount(0);
+//     }
+    
+//   return (
+//     <div>
+//         <h1>{count}</h1>
+//         <div className="btns">
+//         <button onClick={dec}>-</button>
+//         <button onClick={reset}>Reset</button>
+//         <button onClick={inc}>+</button>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Counter
+
+
 import { useState } from "react"
 import "./Counter.css"
 const Counter = () => {
